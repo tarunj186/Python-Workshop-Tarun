8b7224249114c7c51cd8f02d6c9f1f57cc2dc2d2
tarunj186 <43946375+tarunj186@users.noreply.github.com>
2018-10-10 13:56:22 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
 print("hello world!!")
 print("Hi, my name is {}".format("Tarun"))
+
+n = 5
+if n < 5:
+    print("n is less than five")
+elif n == 5:
+    print("n is equal to five")
+else:
+    print("n is greater than five")
+    
